d5cfa9d15f1ae3eed2bad366f61bf2beefee2d3c
Goldpm <lowedavidadam@gmail.com>
2019-10-30 14:25:57 -0400

    hooked up inputs

--- javascript.js
+++ javascript.js
@@ -1,9 +1,9 @@
 // https://api.nytimes.com/svc/search/v2/articlesearch.json?q=election&api-key=yFYR8aMLojbJbkXNqaCi4kS0HdHLDRUb
 $('#submitBtn').click(function (e) {
     e.preventDefault();
-    var searchQuery = $('#searchParam')
-    var startYear = $('#start')
-    var endYear = $('#end')
+    var searchQuery = $('#searchTerm')
+    var startYear = $('#startYear')
+    var endYear = $('#endYear')
     var dateQuery = '&begin_date=' + startYear + '&'
     var dateQuery2 = '&end_date=' + endYear + '&'
     var queryURL = 'https://api.nytimes.com/svc/search/v2/articlesearch.json?q=' + searchQuery + '&' + dateQuery + dateQuery2 + 'api-key=yFYR8aMLojbJbkXNqaCi4kS0HdHLDRUb'
